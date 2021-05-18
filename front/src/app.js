8d776e9d3fe1ab4741c329297ef62f284be58d36
Andrey <andrey.grasev@gmail.com>
2021-05-18 13:58:16 +0700

    Initial commit

---
+++ front/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Loader, Navbar, Nav, Icon, Dropdown } from 'rsuite';
+
+import 'rsuite/dist/styles/rsuite-default.css';
+import './app.css';
+
+function App() {
+  return <div className="App">Hello World!</div>;
+}
+
+export default App;
